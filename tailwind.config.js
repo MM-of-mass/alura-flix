f81ea9e28743fc9102b0abebc55b632eb88f0434
Maciel Azevedo <maciel.az.neto@gmail.com>
2023-05-31 23:08:40 -0300

    Colors and typography style config added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,37 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'color-primary-medium': '#2A7AE4',
+        'color-frontend': '#6BD1FF',
+        'color-backend': '#69953B',
+        'color-mobile': '#FFBA05',
+        'color-ux': '#DC6EBE',
+        'color-infra': '#9CD33B',
+        'color-marketing': '#6B5BE2',
+        'color-inovation': '#FF8C2A',
+        'color-data-science': '#9CD33B',
+        'color-black-dark': 'rgba(0, 0, 0, 0.9)',
+        'color-gray-medium': 'rgba(0, 0, 0, 0.6)',
+        'color-gray-light': 'rgba(0, 0, 0, 0.5)',
+        'color-gray-lighter': '#9E9E9E',
+        'color-gray-dark': '#C2C2C2',
+        'color-gray-medium': '#E5E5E5',
+        'color-gray-light': '#F5F5F5',
+        'color-error-dark': '#C62828',
+        'color-error-medium': '#E53935',
+        'color-error-light': '#FCE7E7',
+      },
+      fontSize: {
+        'title-big': '3.75rem',
+        'title-medidum': '2.875rem',
+        'title-small': '2.1875rem',
+        'body-big': '1.6875rem',
+        'body-medium': '1.125rem',
+        'body-small': '1rem',
+        'body-smaller': '0.75rem',
       },
     },
   },
   plugins: [],
-}
+};
